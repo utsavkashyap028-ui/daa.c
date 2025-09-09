08e4d075e8af0f97b4e32036b6d49e8c005e50c9
Utsav Kashyap <utsavkashyap028@gmail.com>
2025-09-09 08:17:09 +0530

    Add files via upload

---
+++ lower.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[] = "hello";
+    int i = 0;
+
+    while (str[i] != '\0') {
+        str[i] = toupper(str[i]); 
+        i++;
+    }
+
+    printf("Uppercase: %s\n", str);
+    return 0;
+}
